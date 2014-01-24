29387f6ec2823fbe8530869a64b32f6c235d39f2
Salem Hilal <salemhilal@gmail.com>
2014-01-24 16:21:45 -0500

    Elaborated on the keys file example.

--- config/keys.example.js
+++ config/keys.example.js
@@ -1,15 +1,14 @@
 module.exports = {
-
+  // Create a new entry for every environment you'd use.
+  // For example, development, production, testing, etc.
   development: {
-    // development config here
-  },
 
-  production: {
-    // production config here
-  },
-
-  test: {
-    // test config here
-  }
-
+    // Here's details for connecting to the MySQL DB. 
+    mysql: {
+      host: "HOST_URL_HERE",
+      user: "USERNAME_HERE",
+      password: "PASSWORD_HERE",
+      database: "DATABASE_HERE",
+      charset: "CHARACTER_ENCODING_HERE"
+    }
 };
