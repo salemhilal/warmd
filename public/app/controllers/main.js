6ecf9c30609ec7d95811f2c93aff30af28c1618f
Salem Hilal <salemhilal@gmail.com>
2014-03-25 19:23:53 -0400

    Navbar is now smart, things are getting prettier

--- public/app/controllers/main.js
+++ public/app/controllers/main.js
@@ -1,12 +1,15 @@
-warmdApp.controller("MainCtrl", ["$scope", "$http", function MainCtrl($scope, $http){
+warmdApp.controller("MainCtrl", ["$scope", "$http", "$location", function MainCtrl($scope, $http, $location){
     // Because templating is easier than html
     $scope.menu = [
-        { name: "My info", url: "/me\" target=\"_blank" },
-        { name: "Shows",   url: "#"},
-        { name: "Log out", url: "/logout" },
-        { name: "Help",    url: "#" },
-        { name: "Home",    url: "#" },
+        { name: "Home",    prefix: "#", route: "/"},
+        { name: "Search",   prefix: "#", route: "/query"},
+        { name: "Log out", prefix: "", route: "/logout" },
     ]
+
+    $scope.isActive = function(location) {
+      console.log($location.path());
+      return location === $location.path();
+    }
 
     $scope.user = {};
 
