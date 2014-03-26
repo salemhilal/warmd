15048d5b1f6b17b040675ab83cedd77d77691f79
Salem Hilal <salemhilal@gmail.com>
2014-03-26 13:06:08 -0400

    Search page looks nice

--- public/app/controllers/query.js
+++ public/app/controllers/query.js
@@ -1,30 +1,32 @@
 warmdApp.controller("QueryCtrl", ["$scope", function QueryCtrl($scope) {
-    console.log("QueryCtrl")
+    console.log("QueryCtrl");
 
-    $scope.artists = [];
+    $scope.types = [
+      {
+        name: "Artists",
+        url: "/artists/query",
+
+      }
+    ]
+
+    $scope.results = [];
+    $scope.toQuery = $scope.types[0];
+
+
 
     // TODO: use $http
     // TODO: Limit query to ten.
     $scope.autocomplete = _.debounce(function(){
-      $.ajax({
-        type: "POST",
-        dataType: "json",
-        url: "/artists/query",
-        data: {
-          query: $scope.query
-        }
-      }).done(function(data){
-        if(data.error) {
-            console.error(data);
-            return;
-        }
-
-        $scope.$apply(function() {
-          $scope.artists = data.filter(function(artist) {
+      $http({ method: "POST", url: $scope.toQuery.url, data: { query: $scope.query.trim() } }).
+        success(function(data, status, headers, config) {
+          $scope.results = data.filter(function(artist) {
             return artist.Artist.trim() != "";
           });
+        }).
+        error(function(data, status, headers, config) {
+          console.error(data);
         });
-      });
+
     }, 200);
 
 }]);
