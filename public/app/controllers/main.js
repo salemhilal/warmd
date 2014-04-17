9a07bf0cf906146c93d48221489f5225ab9b7c16
Salem Hilal <salemhilal@gmail.com>
2014-04-17 12:36:12 -0400

    Added automatic login redirection
    If you were going somewhere and were then asked to login, warmd now
    persists where you were going and then rerouts you to the right place
    once you've logged in

--- public/app/controllers/main.js
+++ public/app/controllers/main.js
@@ -1,26 +1,32 @@
 warmdApp.controller("MainCtrl", ["$scope", "$http", "$location", function MainCtrl($scope, $http, $location){
-    // Because templating is easier than html
-    $scope.menu = [
-      { name: "Home",    prefix: "#", route: "/"},
-      { name: "Search",  prefix: "#", route: "/query"},
-      { name: "Log out", prefix: "",  route: "/logout" },
-    ];
 
-    $scope.isActive = function(location) {
-      return location === $location.path();
-    };
+  // Because templating is easier than html
+  $scope.menu = [
+    { name: "Home",    prefix: "#", route: "/"},
+    { name: "Search",  prefix: "#", route: "/query"},
+    { name: "Log out", prefix: "",  route: "/logout" },
+  ];
 
-    $scope.user = {};
+  $scope.isActive = function(location) {
+    return location === $location.path();
+  };
 
-    // Populate the page with data
-    $http({method: 'GET', url: '/me'}).
-      success(function(data, status, headers, config) {
-        console.log(data);
-        $scope.user = data;
-      }).
-      error(function(data, status, headers, config) {
-        // TODO: Display a "DB is down lol" page here.
-        console.error(data, status, headers, config);
-      });
+  $scope.user = {};
 
+  // Populate the page with data
+  $http({method: 'GET', url: '/me'}).
+    success(function(data, status, headers, config) {
+      console.log(data);
+      $scope.user = data;
+    }).
+    error(function(data, status, headers, config) {
+      // TODO: Display a "DB is down lol" page here.
+      console.error(data, status, headers, config);
+    });
+
+  // Were we supposed to go somewhere?
+  var goto = localStorage.getItem("warmd_goto_url");
+  if(goto) {
+    $location.path(goto);
+  }
 }]);
