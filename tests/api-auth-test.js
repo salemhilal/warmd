2d4f3d5ec7858284c6e8551fe5c7fb406de17ad0
Matthew Baron <mbaron50@gmail.com>
2014-04-21 14:34:28 -0400

    present state of debugging tests

--- tests/api-auth-test.js
+++ tests/api-auth-test.js
@@ -1,11 +1,11 @@
- 
+
 var request = require('request'),
     vows = require('vows'),
     assert = require('assert'),
     apiUrl = "https://127.0.0.1:3000",
     cookie = null;
- 
- 
+
+
 var apiTest = {
   general: function( method, url, data, cb ){
     //console.log( 'cb?', cb )
@@ -16,73 +16,68 @@ var apiTest = {
         json: data || {},
         headers: {Cookie: cookie}
       },
-      function(req, res){
-        cb( res )
+      function(error, response, body){
+        cb( error, response, body )
       }
     )
   },
-  get: function( url, data, cb  ){ apiTest.general( 'GET', url, data, cb    )  },
+  get: function( url, data, cb  ){ apiTest.general( "GET", url, data, cb    )  },
   post: function( url, data, cb ){ apiTest.general( 'POST', url, data, cb   )  },
   put: function( url, data, cb  ){ apiTest.general( 'PUT', url, data, cb    )  },
   del: function( url, data, cb  ){ apiTest.general( 'DELETE', url, data, cb )  }
 }
- 
+
 function assertStatus(code) {
-  return function (res, b, c) {
-    assert.equal(res.statusCode, code);
+  return function (e, res) {
+    assert.equal(res.status, code);
   };
 }
- 
- 
+
+
 function assertJSONHead(){
-  return function(res, b, c ){
+  return function(error, res, body){
     assert.equal( res.headers['content-type'], 'application/json; charset=utf-8' )
   }
 }
- 
+
 function assertValidJSON(){
-  return function(res, b ){
+  return function(error, res, body){
     // this can either be a Object or Array
-    assert.ok( typeof(res.body) == 'object' )
+    assert.typeof(body,'Object' || 'Array' )
   }
 }
- 
+
 var suite = vows.describe('API Localhost Authenticated Tests')
- 
+
 // Very first test!
 .addBatch({
   "Server should be responding": {
     topic: function(){
-      apiTest.get('/ping', {} ,this.callback )
+      request.get('https://127.0.0.7:3000/ping',this.callback);
     },
- 
-    '/ping should repond pong' : function(res, b){
-      assert.ok(res.body)
-      
+
+    '/ping should respond pong' : function(error, res, body){
+      console.log(res);
+      assert.ok(body)
+
     }
   }
 })
- 
+
 .addBatch({
   'Authenticate to /login': {
     topic: function(){
-      request.post(
-        {
-          url: "https://127.0.0.1:3000/user/session",
-          json: {  username: 'mcbaron', password: 'notMyPassword' }
-        },
+      request.post("https://127.0.0.1:3000/user/session",
         this.callback
-      );
+      ).form({username: 'mcbaron', password: 'bacon'});
     },
- 
- 
- 
-    'get a valid Cookie': function(req, res, body, err){
+
+    'get a valid Cookie': function(error, res, body){
       try{
         cookie = res.headers['set-cookie'].pop().split(';')[0]
         console.log("GOT COOKIE!", cookie)
-      } catch(e){ }
- 
+      } catch(error){ }
+
       assert.ok( typeof(cookie) == 'string' && cookie.length > 10 )
     }
   }
@@ -90,25 +85,24 @@ var suite = vows.describe('API Localhost Authenticated Tests')
 .addBatch({
   'Pronobozo Ze1eE': {
     topic: function(){
-      apiTest.get('app/#/albums/42176', {}, this.callback)
+      apiTest.get('/app/#/albums/42176', {}, this.callback)
     },
     'should be 200': assertStatus(200),
     'should have JSON header' : assertJSONHead(),
     'body is valid JSON' : assertValidJSON(),
- 
+
   },
 })
 .addBatch({
   'Search': {
     topic: function(){
-      apiTest.get('app/#/query', {}, this.callback)
+      apiTest.get('/app/#/query', {}, this.callback)
     },
     'should be 200': assertStatus(200),
     'should have JSON header' : assertJSONHead(),
     'body is valid JSON' : assertValidJSON(),
- 
+
   },
 })
- 
-//suite.run( )
-suite.export( module )
+
+.export( module )
