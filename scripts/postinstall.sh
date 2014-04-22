1dcec574b8e6254ae646bd5a32d76962af49054a
Salem Hilal <salemhilal@gmail.com>
2014-04-22 14:21:35 -0400

    Moved postinstall to a script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-export PATH=$PATH:./node_modules/.bin
+# Enhance path with node binaries
+# export PATH=$PATH:./node_modules/.bin
+
+# Make sure we're in the script directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
+
+# Make log folder
+mkdir -p ../logs
