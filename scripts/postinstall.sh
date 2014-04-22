d6fd519dd3ca93d837a11667980c5e400daac276
Salem Hilal <salemhilal@gmail.com>
2014-04-22 16:48:30 -0400

    Set everything up for the sql dump

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-export PATH=$PATH:./node_modules/.bin
+# Enhance path with node binaries
+# export PATH=$PATH:./node_modules/.bin
+
+# Make sure we're in the script directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $SCRIPT_DIR
+
+# Make log folder
+mkdir -p ../logs
+
+# Copy keys if we're on travis so that warmd knows where to look
+if [ "$TRAVIS" = true ]; then
+	cp ../config/keys.example.js ../config/keys.js
+fi
