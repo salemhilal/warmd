24b06cb6b6078f40b4b43c55b33d5d1554ec6fd8
Salem Hilal <salemhilal@gmail.com>
2014-04-24 12:32:14 -0400

    Cleaned up landing page a li'l

--- public/app/controllers/home.js
+++ public/app/controllers/home.js
@@ -1,9 +1,21 @@
+/* globals warmdApp */
+"use strict";
+
 warmdApp.controller("HomeCtrl", ["$scope", "$http", function ($scope, $http) {
 
   console.log("HomeCtrl");
 
   $scope.showProgram = function(program) {
     return (program.StartTime != program.EndTime);
+  };
+
+  $scope.getProgramTime = function(program) {
+    var daysOfTheWeek = ["Sundays", "Mondays", "Tuesdays", "Wednesdays", "Thursdays", "Fridays", "Saturdays"];
+
+    var start = new Date(program.StartTime);
+    // var end = new Date(program.endTime);
+
+    return daysOfTheWeek[start.getDay()] + " at " + start.toLocaleTimeString();
   };
 
 }]);
