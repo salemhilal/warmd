ae89d5e68d2551b000a162e06cd126e1d766114c
Salem Hilal <salemhilal@gmail.com>
2014-05-26 23:50:28 -0400

    Fail less hard on postinstall when certs aren't generated

--- scripts/gencerts.sh
+++ scripts/gencerts.sh
@@ -21,22 +21,22 @@ KEYFILE=$KEYDIR/server-key.pem
 
 if [ ! -d $CERTDIR ]; then
   echo "$SSLDIR/certs directory doesn't exist"
-  exit 1
+  exit 0
 fi
 
 if [ ! -d $KEYDIR ]; then
   echo "$SSLDIR/private directory doesn't exist"
-  exit 1
+  exit 0
 fi
 
 if [ -f $CERTFILE ]; then
   echo "$CERTFILE already exists, won't overwrite"
-  exit 1
+  exit 0
 fi
 
 if [ -f $KEYFILE ]; then
   echo "$KEYFILE already exists, won't overwrite"
-  exit 1
+  exit 0
 fi
 
 $OPENSSL req -new -x509 -nodes -config $OPENSSLCONFIG -out $CERTFILE -keyout $KEYFILE -days 365 || exit 2
